b797f919553063ed26b71ec4390655f2c248154d
kevin <kevinrangels>
2021-01-31 13:26:01 -0500

    modal control images project in dashboard

--- frontend/components/Dashboard/Projects/ModalImagesProject.vue
+++ frontend/components/Dashboard/Projects/ModalImagesProject.vue
@@ -3,7 +3,69 @@
     <b-modal v-model="modalShow" id="imgProject" centered size="lg" :hide-footer="true" :hide-header="true" body-class="modalBody">
         <h3 class="ModalAddTech__title mb-3">Control Images</h3>
         <div class="projectsImgs__container">
-          {{project}}
+          <div class="projectsImgs__sectionMode mb-3">
+            <div class="projectsImgs__containerCheckOrder">
+              <input type="checkbox" id="order" value="order" v-model="selectType" @change="handleSelectType($event)">
+              <label for="order">Order</label>
+            </div>
+            <div class="projectsImgs__containerCheckPoster">
+              <input type="checkbox" id="poster" value="poster" v-model="selectType" @change="handleSelectType($event)">
+              <label for="poster">Poster</label>
+            </div>
+          </div>
+          <div class="projectsImgs__containerImages mb-2" v-if="project !== null">
+            <label class="projectsImgs__containeImage"
+              v-for="(image, index) in project.images"
+              :key="index"
+              :id='image'
+            >
+              <img :src='image' alt="" class="img-fluid">
+              <input
+                v-if="selectType[0] === 'order'"
+                type="checkbox"
+                v-model="imageSelected"
+                :id='image'
+                :value='image'
+                class="d-none"
+              >
+              <input
+                v-if="selectType[0] === 'poster'"
+                type="checkbox"
+                v-model="imageSelectedPoster"
+                @change="handleSelectPoster($event)"
+                :id='image'
+                :value='image'
+                class="d-none"
+              >
+            </label>
+          </div>
+          <div class="projectsImgs__containerEnd">
+            <div class="projectsImgs__containerPoster">
+              <h3 class="ModalAddTech__title mb-3">Poster</h3>
+              <div class="projectsImgs__containeImage">
+                <img v-if="imageSelectedPoster.length > 0" :src='imageSelectedPoster[0]' alt="" class="img-fluid">
+              </div>
+            </div>
+            <div class="projectsImgs__containerOrder">
+              <h3 class="ModalAddTech__title mb-3">Order</h3>
+              <div class="projectsImgs__containerImgsOrder">
+                <div class="projectsImgs__containeImage"
+                  v-for="(image, index) in imageSelected"
+                  :key="index"
+                >
+                  <img :src='image' alt="" class="img-fluid">
+                  <span class="projectImgs__position">{{index + 1}}</span> 
+                </div>
+              </div>
+            </div>
+              
+          </div>
+            <div class="projectImgs__containerBtns w-100 mt-3">
+              <button class="dashboard__mainBtn">
+                save
+              </button>
+            </div>
+          <!-- <span class="text-white">{{imageSelected}}</span> -->
         </div>
     </b-modal>
   </div>
@@ -25,7 +87,10 @@ export default {
   },
   data() {
     return {
-      modalShow: false
+      modalShow: false,
+      selectType: [],
+      imageSelected: [],
+      imageSelectedPoster: []
     };
   },
   mounted() {
@@ -36,6 +101,14 @@ export default {
     },
     closeModal () {
       this.modalShow = false
+    },
+    handleSelectType (e) {
+      this.selectType = []
+      this.selectType.push(e.target.value)
+    },
+    handleSelectPoster (e) {
+      this.imageSelectedPoster = []
+      this.imageSelectedPoster.push(e.target.value)
     }
   },
   watch:{
@@ -85,5 +158,89 @@ export default {
     outline: none;
     color: gold;
 }
-
+.projectsImgs__container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.projectsImgs__sectionMode {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+}
+.projectsImgs__containerCheckOrder {
+  width: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.projectsImgs__containerCheckOrder label {
+  color: #ffffff;
+  margin-left: 8px;
+  margin-bottom: 0;
+}
+.projectsImgs__containerCheckPoster {
+  width: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.projectsImgs__containerCheckPoster label {
+  color: #ffffff;
+  margin-left: 8px;
+  margin-bottom: 0;
+}
+.projectsImgs__containerImages {
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  overflow-x: auto;
+}
+.projectsImgs__containeImage {
+  width: 120px;
+  height: 120px;
+  display: flex;
+  margin-right: 15px;
+  position: relative;
+  cursor: pointer;
+}
+.projectImgs__position {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: gold;
+  color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: -8px;
+  right: -8px;
+  font-weight: bold;
+}
+.projectsImgs__containerEnd {
+  width: 100%;
+  display: flex;
+}
+.projectsImgs__containerPoster {
+  width: 30%;
+  border: 2px solid gold;
+  padding: 10px;
+}
+.projectsImgs__containerOrder {
+  width: 70%;
+  border: 2px solid gold;
+  padding: 10px;
+}
+.projectsImgs__containerImgsOrder {
+    display: flex;
+    flex-wrap: wrap;
+    height: 300px;
+    overflow-y: auto;
+    justify-content: space-between;
+    padding: 10px;
+}
+.projectImgs__containerBtns {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
